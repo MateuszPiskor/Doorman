ccd3616c415e83adee06f3ac46c07c0de2f11135
Mateusz Mati <grytychy@gmail.com>
2021-01-22 12:27:44 +0100

    test

---
+++ DoorMan.DataAccess/DoorManDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Doorman.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoorMan.DataAccess
+{
+    public class DoormanDBContext : DbContext
+    {
+        public DoormanDBContext() : base("DoormanDb")
+        {
+
+        }
+
+        public DbSet<Key> Keys { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+                    
+        }
+    }
+}
---
+++ Doorman/Models/Key.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doorman.Models
+{
+    public class Key
+    {
+        public int Id { get; set; }
+        public int RoomNumber { get; set; }
+        public string RoomName { get; set; }
+    }
+}
