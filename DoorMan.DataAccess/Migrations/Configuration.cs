62135f10befa9c0bf01dee8886ef564fb46450fa
Mateusz Mati <grytychy@gmail.com>
2021-01-22 13:21:01 +0100

    Create and seed DB

--- DoorMan.DataAccess/Migrations/Configuration.cs
+++ DoorMan.DataAccess/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace DoorMan.DataAccess.Migrations
 {
+    using Doorman.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -19,12 +20,19 @@ namespace DoorMan.DataAccess.Migrations
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data. E.g.
             //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
+            context.Keys.AddOrUpdate(
+              p => p.RoomNumber,
+              new Key { RoomNumber=101, RoomName = "Room1", },
+              new Key { RoomNumber=102, RoomName = "Room2", },
+              new Key { RoomNumber= 103, RoomName = "Room3", },
+              new Key { RoomNumber=104, RoomName = "Room4", },
+              new Key { RoomNumber=105, RoomName = "Room5", },
+              new Key { RoomNumber=106, RoomName = "Room6", },
+              new Key { RoomNumber=107, RoomName = "Room7", },
+              new Key { RoomNumber=108, RoomName = "Room8", },
+              new Key { RoomNumber=109, RoomName = "Room9", },
+              new Key { RoomNumber=110, RoomName = "Room10", }
+            );
             //
         }
     }
