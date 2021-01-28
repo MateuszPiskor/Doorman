a22314de9821664dad130572266cfdb2f5109405
Mateusz Mati <grytychy@gmail.com>
2021-01-28 10:10:21 +0100

    Add extension method changing int id to string with initial zeros

---
+++ Doorman.Tests/HelpersTests/IntExtensionsTests.cs
@@ -0,0 +1,41 @@
+using Autofac;
+using Doorman.DataServices;
+using Doorman.StartUp;
+using DoorMan.DataAccess;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using System.Linq;
+using Doorman.Model;
+using Doorman.Helpers;
+
+namespace Doorman.Tests
+{
+    public class IntExtensionsTests
+    {
+        [Fact]
+        public void ShouldReturnThreeInitialZero ()
+        {
+            int i = 1;
+            string numberWithInitialZZeros= i.ChangeIdToIdWithInitialZeros();
+            Assert.Equal("0001", numberWithInitialZZeros);
+        }
+
+        [Fact]
+        public void ShouldReturnTwoInitialZero()
+        {
+            int i = 34;
+            string numberWithInitialZZeros = i.ChangeIdToIdWithInitialZeros();
+            Assert.Equal("0034", numberWithInitialZZeros);
+        }
+
+        [Fact]
+        public void ShouldReturnNumberAsAString()
+        {
+            int i = 1234;
+            string numberWithInitialZZeros = i.ChangeIdToIdWithInitialZeros();
+            Assert.Equal("1234", numberWithInitialZZeros);
+        }
+
+    }
+}
---
+++ Doorman/Helpers/IntExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doorman.Helpers
+{
+    public static class IntExtensions
+    {
+        public static string ChangeIdToIdWithInitialZeros(this int number)
+        {
+            string IdWithInitialZeros = "";
+
+            string numerAsAString= number.ToString();
+
+            IdWithInitialZeros += numerAsAString;
+
+            while (IdWithInitialZeros.Length < 4)
+            {
+                IdWithInitialZeros = IdWithInitialZeros.Insert(0, "0");
+            }
+            return IdWithInitialZeros;
+        }
+
+
+    }
+}
